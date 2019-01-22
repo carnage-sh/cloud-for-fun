6e9a69a989b0699b38310e5db42a31097463cf02
Gregory Guillou <gregory.guillou@resetlogs.com>
2019-01-22 17:33:11 +0100

    Remove Kubeadm episode (#2)

--- episode-002/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-  default = "eu-west-1"
-}
-
-variable "sshkey" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDLqoFOCVbo2la8AOnN2PvYMCbPsBx8y51CT1Jx2wjbQoyqR81IgOSYCH/+aGv3XD6nDDdzSoVRcv5ddMisWWet7DcXSbjfoACIFTpa2rWcNCwe3q6VUmD7kYlHLQyc/GGJJZ0iir0fCmD2pKNYWYEKimijzJeE4oSeLHlLOB2XShNmuCx4i58HAcQdTR2H+PRZfu3lUNLZ7AF4FATrzz7keULnTQj9bCSk+ThSnm3V6LPHCPSDyOhW8q/Z5b3+URYM7b/WAVnbVq0UVVAbupKrJHn+WFBlLcgCcvILkWpRXg0HU7JbXprKexECGW60zd1LS3R1NkOLK131mWfHDpl"
-}
