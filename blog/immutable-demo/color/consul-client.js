51809ed626d69a8435025e2ecdee7ce2b965b685
Gregory Guillou <gregory.guillou@resetlogs.com>
2019-01-24 11:35:49 +0100

    Autodiscover consul

--- blog/immutable-demo/color/consul-client.js
+++ blog/immutable-demo/color/consul-client.js
@@ -2,6 +2,8 @@ const ip = require('ip')
 const os = require('os')
 const moment = require('moment')
 const request = require('request')
+const process = require('process')
+const consul = process.env.CONSUL_HOSTNAME || "localhost"
 
 function updateService() {
   const date = new Date();
@@ -15,16 +17,14 @@ function updateService() {
 
   request({
    method: 'PUT',
-   uri: "http://192.168.1.146:8500/v1/agent/service/register",
-   multipart: [{
-       'content-type':'application/json',
-       body: JSON.stringify(register)
-   }]}, function(error, request, body) {
+   uri: `http://${consul}:8500/v1/agent/service/register`,
+   body: JSON.stringify(register)
+   }, function(error, response, body) {
   if (error == null) {
     return console.log(
         `${ip.address()} - - ` +
         `[${moment(date).utc().format('DD/MMM/YYYY:HH:mm:ss ZZ')}] ` +
-        `"REGISTER ${os.hostname()}:8000" 200 0 "-" ""`)
+        `"REGISTER ${os.hostname()}:8000" ${response.statusCode} 0 "-" ""`)
   }
   console.log(
       `${ip.address()} - - ` +
