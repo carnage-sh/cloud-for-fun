ed8b1cb446638307f56f252610d0ac489a498f63
Gregory Guillou <gregory.guillou@resetlogs.com>
2019-05-01 19:51:33 +0200

    Add files

--- blog/knative/test.sh
+++ blog/knative/test.sh
@@ -1,2 +1,15 @@
-curl -H 'Host: helloworld-go.default.example.com' a2f760ed56ab511e995b0069b3f28ec6-1656798546.eu-west-1.elb.amazonaws.com
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
+while true; do
+    sleep 1
+    OUTPUT=$(curl --silent -H 'Host: helloworld-go.default.example.com' a2f760ed56ab511e995b0069b3f28ec6-1656798546.eu-west-1.elb.amazonaws.com)
+if [[ "$OUTPUT" == "Hello v2!" ]]; then
+   echo "${green}${OUTPUT}${reset}"
+elif [[ "$OUTPUT" == "Hello v1!" ]]; then
+    echo "${red}${OUTPUT}${reset}"
+else
+    echo "${OUTPUT}"
+fi
+done
